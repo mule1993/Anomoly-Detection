f27d56cb65a5624c762fb74fce6e717caeb397fc
mule1993 <71659980+mule1993@users.noreply.github.com>
2026-02-07 12:00:04 +0300

    Implement basic FastAPI structure for anomaly detection

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI(title="Anomaly Detection Service")
+
+# This defines what the 'input' to our model looks like
+class Transaction(BaseModel):
+    transaction_id: str
+    amount: float
+
+@app.get("/")
+def home():
+    return {"status": "Model is Online", "version": "0.1.0-baseline"}
+
+@app.post("/predict")
+def predict(data: Transaction):
+    # THE INFRASTRUCTURE RULE: 
+    # Label everything as 0 (No Anomaly) for now.
+    return {
+        "transaction_id": data.transaction_id,
+        "prediction": 0,
+        "model_version": "baseline_constant_zero"
+    }
